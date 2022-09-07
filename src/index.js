18a27db6403c8b60d79cf7bb42db22abade97a74
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-07 11:28:30 -0400

    mock cc submission

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// write your code here
+fetch ('http://localhost:3000/ramens')
+.then ((res) => res.json())
+.then((json) => console.log(json))
+
+document.querySelector('#div') = ramenMenu.append();
+
+let ramenImagesDiv =  document.createElement('div');
+ramenImagesDiv.className = "card"
+
+// let ramenMenu = document.getElementById("ramen-menu")
+// let ramen_card = document.createElement("div");
+// ramen_card.className = "card"
+// ramenMenu.append(ramen_card)
+
+btn.addEventListener(''), (e) => {
+    e.preventDefault();
+}
+  
+  document.addEventListener('DOMContentLoaded', init);
