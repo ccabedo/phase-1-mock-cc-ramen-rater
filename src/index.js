fc09da152dc17a5b84a98a2b3d3f7894cb05d1cc
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-07 11:14:03 -0400

    commit tree 5f9d2490ed462d67f775bff8e6b4f474181a9702

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// write your code here
+fetch ('http://localhost:3000/ramens')
+.then ((res) => res.json())
+.then((json) => console.log(json))
+
+
+let obj = JSON.parse(json);
+
+
+// document.querySelector()
+
+// cons 
+// let ramenMenu = document.getElementById("ramen-menu")
+// let ramen_card = document.createElement("div");
+// ramen_card.className = "card"
+// ramenMenu.append(ramen_card)
+
+
+// button.addEventListener('click'), () => {
+//     console.log('button clicked', )
+//     document.body.appendChild(button)}
+
+
+const init = () => {
+    const inputForm = document.querySelector('form');
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      console.log(event);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
