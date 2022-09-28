28d8932506a26d5c88c139c662a0f4df84d9be1d
Colleen Cabedo <101096343+ccabedo@users.noreply.github.com>
2022-09-28 11:34:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,57 @@
 fetch ('http://localhost:3000/ramens')
-.then ((res) => res.json())
-.then((json) => console.log(json))
+.then (res => res.json())
+.then (data => {
+    data.forEach(ramen => {
+        const div =  document.querySelector("#ramen-menu")
+        const img = document.createElement("img")
+        img.src = ramen.image
+        div.append(img)
 
-document.querySelector('#div') = ramenMenu.append();
+        img.addEventListener("click", e => {
+            const ramenDetail = document.querySelector("#ramen-detail")
+            const ramenImage = document.querySelector(".detail-image")
+            ramenImage.src = ramen.image
 
-let ramenImagesDiv =  document.createElement('div');
-ramenImagesDiv.className = "card"
+            const name = document.querySelector(".name")
+            name.textContent = ramen.name
 
-// let ramenMenu = document.getElementById("ramen-menu")
-// let ramen_card = document.createElement("div");
-// ramen_card.className = "card"
-// ramenMenu.append(ramen_card)
+            const restaurant = document.querySelector(".restaurant")
+            restaurant.textContent = ramen.restaurant
 
-btn.addEventListener(''), (e) => {
-    e.preventDefault();
-}
-  
-  document.addEventListener('DOMContentLoaded', init);
+            ramenDetail.append(ramenImage, name, restaurant)
+
+            const rating = document.querySelector("#rating-display")
+            rating.textContent = ramen.rating
+
+            const comment = document.querySelector("#comment-display")
+            comment.textContent = ramen.comment
+
+const form = document.querySelector("#new-ramen")
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const inputName = document.querySelector("#new-name").value
+    const inputRestaurant = document.querySelector("#new-restaurant").value
+    const inputImage = document.querySelector("#new-image").value
+    const inputRating = document.querySelector("#new-rating").value
+    const inputComment = document.querySelector("#new-comment").value
+    
+    fetch("http://localhost:3000/ramens", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            accept: "application/json"
+            },
+        body: JSON.stringify({
+            name: inputName,
+            restaurant: inputRestaurant,
+            image: inputImage,
+            rating: inputRating,
+            comment: inputComment
+        })
+        }
+    )
+})
+})
+})
+})
